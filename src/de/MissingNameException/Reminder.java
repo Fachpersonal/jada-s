0ed3d335ea79dd24193416b59e256cb9d3bc6bf4
falscherIdiot <falscherIdiot@gmail.com>
2021-01-28 22:00:17 +0100

    reworked showReminder() added removeReminder()

--- src/de/MissingNameException/Reminder.java
+++ src/de/MissingNameException/Reminder.java
@@ -1,11 +1,15 @@
 package de.MissingNameException;
 
-import java.time.LocalDate;
+import java.io.IOException;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Reminder {
 
+	private int reminderID;
+	
 	private String reminderLabel;
 	
 	private String reminderDescription;
@@ -14,20 +18,33 @@ public class Reminder {
 	
 	private ClientHandler client;
 	
-	public static ArrayList<Reminder> reminders = new ArrayList<Reminder>();
-	
-	public Reminder(String reminderLabel, String reminderDescription, LocalDate reminderDate, ClientHandler client) {
+	public Reminder(String reminderLabel, String reminderDescription, LocalDate reminderDate, ClientHandler client) throws IOException {
 		this.reminderLabel = reminderLabel;
 		this.reminderDescription = reminderDescription;
 		this.reminderDate = reminderDate;
 		this.client = client;
-		reminders.add(this);
+		ArrayList<String> x = App.d.StringSELECT("SELECT * FROM jada.reminder", "id");
+		int i = 0;
+		while(x.contains(String.valueOf(i))) {
+			i++;
+		}
+		reminderID = i;
+		App.d.execute("INSERT INTO jada.reminder (id, label, description, date, user) VALUE ('" + reminderID + "','" + reminderLabel + "','" + reminderDescription + "','" + reminderDate.getDayOfMonth() + "." + reminderDate.getMonthValue() + "." + reminderDate.getYear() + "','" + client.getAccountName() + "'");
+		App.log.add("@" + LocalTime.now().getHour() + ":" + LocalTime.now().getMinute() + " Client 'SYSTEM' :: Created reminder '" + reminderLabel + "'");
 	}
 
-	public static void showReminders() {
-		for (int i = 0; i < reminders.size(); i++) {
-			System.out.println(reminders.get(i).getReminderLabel() + "," + reminders.get(i).getReminderDescription() + "," + reminders.get(i).getReminderDate().getDayOfMonth() + "." + reminders.get(i).getReminderDate().getMonthValue() + "." + reminders.get(i).getReminderDate().getYear() + "," + reminders.get(i).getClient());
+	public static void showReminders(ClientHandler client) throws IOException {
+		ArrayList<String> reminder = App.d.StringSELECT("SELECT * FROM jada.reminder WHERE user='" + client.getAccountName() + "'", "id", "label" + "description" + "date"); 
+		String x = "";
+		for (int i = 0; i < reminder.size(); i+=5) {
+			x += reminder.get(i) + " | " + reminder.get(i+1) + " | " + reminder.get(i+2) + " | " + reminder.get(i+3) + " | " + reminder.get(i+4) + App.nl;
 		}
+		App.printC(client.getClientSocket(), x);
+	}
+	
+	public static void removeReminder(int reminderID) throws IOException {
+		App.d.execute("DELETE FROM jada.reminder WHERE id= " + reminderID);
+		App.log.add("@" + LocalTime.now().getHour() + ":" + LocalTime.now().getMinute() + " Client 'SYSTEM' :: Reminder removed (=ID)'" + reminderID + "'");
 	}
 	
 	public String getReminderLabel() {return reminderLabel;}
